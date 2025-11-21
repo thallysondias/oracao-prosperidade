52e44a81de0974d86279f0a20fb471193f2f25f8
Thallyson dias <thallysondias@gmail.com>
2025-11-21 17:20:38 +0000

    routa es

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,7 +3,7 @@ import {createNavigation} from 'next-intl/navigation';
 
 export const routing = defineRouting({
   locales: ['pt', 'en', 'es'],
-  defaultLocale: 'pt'
+  defaultLocale: 'es'
 });
 
 export const {Link, redirect, usePathname, useRouter} =
